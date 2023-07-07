f269b55c7aae4211083d0acb188f9e0c2e0f357d
camcasuga <carlislemarquez.casuga@gmail.com>
2023-07-07 16:48:34 +0300

    more runs

--- submitmv5.sh
+++ submitmv5.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-OMP_NUM_THREADS=2 ../../Downloads/practice/rcbk/build/bin/rcbk -ic MV ${1} ${2} 0.01 ${3} -rc BALITSKY -alphas_scaling ${4} -maxy 12 -fast -output results/bks/${5}.dat
+#SBATCH -N 1
+#SBATCH -n 12
+#SBATCH -t 12:00:00
+### SBATCH -o log_py.txt
+
+OMP_NUM_THREADS=12 ../../Downloads/practice/rcbk/build/bin/rcbk -ic MV ${1} ${2} 0.01 ${3} -rc BALITSKY -alphas_scaling ${4} -maxy 12 -fast -output results/bks/${5}.dat
